29d7c768c89f960ea020ae4c2ab0424ca4ac3efe
Priyam Pradhan <priyampradhan07@gmail.com>
2025-02-11 18:07:58 +0530

    second

---
+++ src/test/java/com/priyam/movieflix/MovieflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyam.movieflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
