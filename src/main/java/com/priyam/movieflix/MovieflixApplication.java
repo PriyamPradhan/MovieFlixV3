fd8b2996e66efbe9ce09deaf865eeca7e7661ba5
Priyam Pradhan <priyampradhan07@gmail.com>
2025-02-11 19:04:01 +0530

    last main commits

--- src/main/java/com/priyam/movieflix/MovieflixApplication.java
+++ src/main/java/com/priyam/movieflix/MovieflixApplication.java
@@ -10,6 +10,9 @@ public class MovieflixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieflixApplication.class, args);
+
+		System.out.println("Application Running fine..");
+		System.out.println("DB connections looks good");
 	}
 
 }
